25413a671b28e5175b20b2beb1ec7917dfd4af88
FaizanAnsari185 <superseupermfa@gmail.com>
2024-11-12 20:58:59 +0530

    Lerning Contetx API

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
-"use client"
-
+"use client";
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 import NavBar from "@/Components/NavBar";
-
+import { MyContext } from "@/Helper/Context";
+import { useContext } from "react";
 
 export default function Home() {
+  const user = useContext(MyContext);
   return (
-   <>
-   <Header />
-   <Footer />
-   <NavBar />
-   </>
+    <>
+      <div className="text-4xl text-blue-500">This is page.js --- {user}</div>
+      <Header />
+      <Footer />
+      <NavBar />
+    </>
   );
 }
